ff9a79f6000439afaabdc1bbe8b7fae384e5bf0e
Vladimir Pirotskiy <vladimir.pirotskiy@gmail.com>
2024-09-10 02:02:52 +0300

    feat(logo): logo component;

---
+++ app/components/Logo.tsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <div className="flex cursor-pointer items-center gap-2">
+      <h1 className="!text-primary font-semibold">VIP.AI.</h1>
+      <h1 className="!bg-secondary text-white px-4 py-1 rounded-full flex justify-center items-center font-semibold">
+        Admin
+      </h1>
+    </div>
+  )
+}
+
+export default Logo
