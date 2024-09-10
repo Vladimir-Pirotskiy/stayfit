1de7f1b51c5dd29417928646241fe139f40e8bb4
Vladimir Pirotskiy <vladimir.pirotskiy@gmail.com>
2024-09-10 03:14:46 +0300

    refactor(loading): formatting;

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center  h-screen ">
+    <div className="flex h-screen flex-col items-center justify-center">
       <div className="spinner-border !text-secondary" />
       <h1 className="!text-secondary">
         Loading<span className="animated-dots !text-primary"></span>
